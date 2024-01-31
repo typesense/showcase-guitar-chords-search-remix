62ec171bd694e0cd777ff4c2df2f241583dbb221
Phi Hung Hoang <hphihung795@gmail.com>
2024-01-31 18:36:46 +0700

    load server env vars into the client-side

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { cssBundleHref } from '@remix-run/css-bundle';
+import type { LinksFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -7,24 +7,52 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-
+  json,
+  useLoaderData,
+} from '@remix-run/react';
+declare global {
+  interface Window {
+    ENV: {
+      PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY: string;
+      PUBLIC_TYPESENSE_HOST: string;
+      PUBLIC_TYPESENSE_PORT: string;
+      PUBLIC_TYPESENSE_PROTOCOL: string;
+    };
+  }
+}
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
 ];
-
+export async function loader() {
+  return json({
+    ENV: {
+      PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY:
+        process.env.PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY,
+      PUBLIC_TYPESENSE_HOST: process.env.PUBLIC_TYPESENSE_HOST,
+      PUBLIC_TYPESENSE_PORT: process.env.PUBLIC_TYPESENSE_PORT,
+      PUBLIC_TYPESENSE_PROTOCOL: process.env.PUBLIC_TYPESENSE_PROTOCOL,
+    },
+  });
+}
 export default function App() {
+  const envVars = useLoaderData<typeof loader>();
+
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
       <body>
         <Outlet />
         <ScrollRestoration />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(envVars.ENV)}`,
+          }}
+        />
         <Scripts />
         <LiveReload />
       </body>
